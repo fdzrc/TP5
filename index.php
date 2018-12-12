743e8f96792b68413f248af8ca2b3e51123c32ec
FERNANDEZ Kevin <45633060+fdzrc@users.noreply.github.com>
2018-12-12 18:07:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
-<!doctype html>
-<head>
-<meta charset=utf-8">
-</head>
-                    <body>
-                    $SESSION_start ();
-<?php
 
-/* SCRIPT FRONTAL AFFICHAGE DE CAPTCHA
-* AFFICHER UNE IMAGE GÉNÉRÉE EN PHP */
+			<!DOCTYPE html>
+<html>
+	
+	<head>
+<meta charset="utf-8">
+	<title>notre captcha</title>
+	
+	</head>
+	<body>
+	<h1>code de sécurité:</h1>
+			<?php
+			session_start();
 
 echo "<img src='script-captchas.php' alt='captchas' />";
 
 ?>
-                    </body>
-</html>
+		<form action ="index.php" method="POST">
+		<label for="text">
+		<input type="text" id="titre" name="titre">
+			<input type="submit" name="envoyer" value="envoyer">
+		</form>
+		</body>
+		</html>
